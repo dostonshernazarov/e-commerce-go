7cd4494e957fbbfdad75317782bc47a3260b1cc3
Oyatillohgayratov <azamatgayratov771@gmail.com>
2024-09-10 12:42:52 +0500

    order has been added

--- order-service/internal/client/user/user.go
+++ order-service/internal/client/user/user.go
@@ -11,25 +11,27 @@ package user
 // 	"google.golang.org/grpc/credentials/insecure"
 // )
 
-// func UserClinet(req int) (int32, error) {
-// 	cfg := config.Config{}
-// 	err := configloader.LoadYAMLConfig("config.yaml", &cfg)
-// 	if err != nil {
-// 		return 0, err
-// 	}
-// 	port := fmt.Sprintf("%s:%s", cfg.UserServer.Http.Host, cfg.UserServer.Http.Port)
+func UserClinet(req int) (int32, error) {
+	// 	cfg := config.Config{}
+	// 	err := configloader.LoadYAMLConfig("config.yaml", &cfg)
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
+	// 	port := fmt.Sprintf("%s:%s", cfg.UserServer.Http.Host, cfg.UserServer.Http.Port)
 
-// 	conn, err := grpc.NewClient(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
-// 	if err != nil {
-// 		return 0, err
-// 	}
+	// 	conn, err := grpc.NewClient(port, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
 
-// 	client := userpb.NewUsrServiceClient(conn)
+	// 	client := userpb.NewUsrServiceClient(conn)
 
-// 	res, err := client.GetUser(context.Background(), &userpb.UserRequest{Id: int32(req)})
-// 	if err != nil {
-// 		return 0, err
-// 	}
+	// 	res, err := client.GetUser(context.Background(), &userpb.UserRequest{Id: int32(req)})
+	// 	if err != nil {
+	// 		return 0, err
+	// 	}
 
-// 	return res.Id, nil
-// }
+	// return res.Id, nil
+	return 0, nil
+
+}
